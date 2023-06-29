2f082209a7eb34a45683ac3457b6d76984e2f082
Tiffany Tu <tiffanytu2024@gmail.com>
2023-06-29 11:15:42 -0700

    Initial Commit

---
+++ JournalTest/JournalTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalTestApp.swift
+//  JournalTest
+//
+//  Created by Scholar on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct JournalTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
